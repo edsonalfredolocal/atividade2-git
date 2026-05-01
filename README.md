f4b45f1527fa9c238bf6d6c6335a820a3f718f0b
Edson Alfredo <alfredoformacao@gmail.com>
2026-05-01 14:05:53 +0100

    Primeira versão do portfólio de scripting

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portfólio de Scripting
+## Disciplina
+Linguagens de Scripting e Linha de Comandos
+
+## Objetivo
+Aprender Bash, Git, GitHub e automação de tarefas.
---
+++ atividade1/meu_script.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+echo "===== INÍCIO DO SCRIPT ====="
+
+echo "Diretoria atual:"
+pwd
+
+echo ""
+echo "Conteúdo da pasta:"
+ls
+
+echo ""
+echo "===== FIM DO SCRIPT ====="

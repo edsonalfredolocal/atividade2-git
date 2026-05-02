ad73b28065ccd4f7893846e363eb6590e564a2d1
edsonalfredo <contasdadosalfredo@outlook.pt>
2026-05-02 20:11:37 +0100

    Melhoria do script monitor

--- atividade1/scripts/script_avancado.sh
+++ atividade1/scripts/script_avancado.sh
@@ -1,19 +1,6 @@
 
 #!/bin/bash
-echo "===== SCRIPT AVANÇADO ====="
 
-echo "Data atual:"
+echo "===== MONITOR DE DIRETÓRIO ====="
 date
-
-echo ""
-echo "Número de ficheiros na pasta:"
-ls | wc -l
-
-echo ""
-echo "A criar ficheiro de log..."
-
-echo "Log criado em: $(date)" >> log.txt
-echo "Total de ficheiros: $(ls | wc -l)" >> log.txt
-
-echo ""
-echo "Log atualizado com sucesso!"
+ls -1 | wc -l
